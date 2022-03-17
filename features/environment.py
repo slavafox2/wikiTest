1aea19a2130668845f64153a0dd6ec35b0e24886
Slav <tyop@tut.by>
2022-03-17 11:05:04 +0300

    env

--- features/environment.py
+++ features/environment.py
@@ -10,7 +10,7 @@ def before_scenario(context, scenario):
         "deviceName": "Android Emulator",
         "automationName": "UiAutomator2",
         "appWaitActivity": "*",
-        "app": r"C:\Users\SL\PycharmProjects\wikiTest\app_apk\wikipedia-2-7-50391-huawei-2022-01-11.apk"
+        "app": r"C:\...\PycharmProjects\wikiTest\app_apk\wikipedia-2-7-50391-huawei-2022-01-11.apk"
     }
 
     context.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_capabilities=desired_capabilities)
